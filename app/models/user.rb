c40052471b900d5ea0dbfe635868c37f180ba29f
EC2 Default User <ec2-user@ip-172-31-42-251.ap-northeast-1.compute.internal>
2024-11-09 08:57:17 +0000

    [Add] コントローラー homes books users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # 画像追加
+  has_one_attached :profile_image
+
+
+
 end
