44b2325749d0214caccd70271e4296824c36ab6b
EC2 Default User <ec2-user@ip-172-31-42-251.ap-northeast-1.compute.internal>
2024-11-09 12:57:30 +0000

    [Add]hommes/top ルートに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
   get 'users/index'
   get 'users/show'
   get 'users/edit'
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'homes/top'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
