08b27b9596ebc754fb8916b1198900bcc2d4570b
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-11-18 10:16:50 +0000

    [Add]画像サイズ変更用imageMagickインストール

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   get 'users/edit'
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit]
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
