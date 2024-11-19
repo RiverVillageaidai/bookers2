59a99fd6c91e521157ef4c429a0568a2c490850a
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-11-19 10:06:27 +0000

    [Fix]sign in時の移行先

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-
+  # Devise のルートが干渉するので　この記述は一番上に書く
+  devise_for :users
+  
   root to: 'homes#top'
   get 'homes/about'
-  
+
   resources :users, only: [:index, :show, :edit, :update]
 
   resources :books, only: [:new, :create, :index, :show, :edit]
 
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
