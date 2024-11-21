4dfe6339c94839326015cc1e38a3f6017e5e3c68
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-11-21 10:24:47 +0000

    [Add]edit機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+  
+    if @book.update(book_params)
+      #  "flash[:notice] = 'メッセージ内容'"　リクエスト間を超えてメッセージを表示するための仕組み
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
