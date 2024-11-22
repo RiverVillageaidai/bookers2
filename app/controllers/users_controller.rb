dfb6bd85ddcdd33c92d4145787ef5552fa9f2c07
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-11-22 10:16:51 +0000

    [Add]users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def edit
@@ -19,17 +20,17 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
+  # def user_params
     # params.require(:user).permit(:name, :profile_image)
-  end
+  # end
 
   # ログインしているユーザー以外の情報は編集できないようにする
-  def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to post_images_path
-    end
-  end
+  # def is_matching_login_user
+  #   user = User.find(params[:id])
+  #   unless user.id == current_user.id
+  #     redirect_to post_images_path
+  #   end
+  # end
 
 end
 
